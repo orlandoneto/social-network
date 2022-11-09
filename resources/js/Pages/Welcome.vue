95bdd67b68fd079d15a822f0a5ed64bf79b45d14
orlandoneto <orlandoneto23@gmail.com>
2022-11-09 09:18:36 -0300

    add all project

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { Head, Link } from "@inertiajs/inertia-vue3";
+
+defineProps({
+    users: Number,
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+    <div class="flex justify-around items-center bg-gray-100 min-h-screen">
+        <div>
+            <img
+                :src="'/storage/images/social-network.png'"
+                alt="Social Network"
+            />
+        </div>
+
+        <div class="text-center">
+            <h1 class="text-4xl font-semibold">Social Network</h1>
+
+            <div v-if="canLogin" class="flex justify-center space-x-6 mt-5">
+                <Link
+                    v-if="$page.props.user"
+                    :href="route('dashboard')"
+                    class="text-lg text-gray-700 underline"
+                    >Dashboard</Link
+                >
+
+                <template v-else>
+                    <Link
+                        :href="route('login')"
+                        class="text-white bg-gray-800 rounded px-3 py-1 hover:bg-gray-500"
+                        >Log in</Link
+                    >
+
+                    <Link
+                        v-if="canRegister"
+                        :href="route('register')"
+                        class="text-white bg-gray-800 rounded px-3 py-1 hover:bg-gray-500"
+                        >Register</Link
+                    >
+                </template>
+            </div>
+            <p class="mt-10">{{ users }} active members</p>
+        </div>
+    </div>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [WelcomeController::class, 'show'])->name('wellcome');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
